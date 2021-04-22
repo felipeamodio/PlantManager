b5b166fecbae5c1ecee8d1de47f447b737d81687
Felipe <fealves.amodio@gmail.com>
2021-04-22 19:58:52 -0300

    updtsss

--- src/pages/PlantsSelect.tsx
+++ src/pages/PlantsSelect.tsx
@@ -3,6 +3,7 @@ import { Text, View, StyleSheet, FlatList } from 'react-native';
 import { EnviromentButton } from '../components/EnviromentButton';
 import {Header} from '../components/Header';
 import { PlantsCardPrimary } from '../components/PlantCardPrimary';
+import {Load} from '../components/Load';
 import api from '../services/api';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
@@ -31,12 +32,25 @@ export function PlantsSelect(){
 
     const [enviroments, setEnviroments] = useState<EnviromentProps[]>([]);
     const [plants, setPlants] = useState<PlantProps[]>([]);
+    const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>([]);
     const [enviromentSelected, setEnviromentSelected] = useState('all');
+    const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1);
+    const [loadingMore, setLoadingMore] = useState(false);
+    const [loadedAll, setLoadedAll] = useState(false);
 
 
 
    function handleEnviromentSelected(environment: string){
         setEnviromentSelected(environment);
+
+        if(environment == 'all')
+            return setFilteredPlants(plants);
+
+        const filtered = plants.filter(plant =>
+            plant.environments.includes(environment));
+
+            setFilteredPlants(filtered);
    }
 
     useEffect(() => {
@@ -56,12 +70,28 @@ export function PlantsSelect(){
 
     useEffect(() => {
         async function fetchPlants() {
-            const {data} = await api.get('plants?_sort=name&_order=asc');
-            setPlants(data);
+            const {data} = await api.get(`plants?_sort=name&_order=asc&_page=${page}&_limit=8`);
+
+            if(!data)
+                return setLoading(true);
+
+            if(page > 1){
+                setPlants(oldValue => [...oldValue, ...data])
+                setFilteredPlants(oldValue => [...oldValue, ...data])
+            }else {
+                setPlants(data);
+                setFilteredPlants(data);
+            }
+            
+            setLoading(false);
+            setLoadingMore(false);
         }
 
         fetchPlants();
     }, [])
+
+    if(loading)
+        return <Load />
 
     return(
         <View style={styles.container}>
@@ -88,7 +118,7 @@ export function PlantsSelect(){
 
             <View style={styles.plants}>
                 <FlatList
-                    data={plants}
+                    data={filteredPlants}
                     renderItem={({item}) => (
                         <PlantsCardPrimary data={item} />
                     )}
